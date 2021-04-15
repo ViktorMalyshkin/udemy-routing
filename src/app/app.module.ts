d8edb751c75eba5d140ffe7104357c4ab96a3bc6
Viktor <malyshkin.viktor@icloud.com>
2021-04-15 18:27:39 +0300

    s11 p143. Redirecting and Wildcard Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,22 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children: [
-    {path: ':id/:name', component: UserComponent}]},
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}],
+  },
   {
     path: 'servers', component: ServersComponent, children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent},
     ],
   },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '** ', redirectTo: '/not-found'},
 
 ];
 
@@ -35,6 +40,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
